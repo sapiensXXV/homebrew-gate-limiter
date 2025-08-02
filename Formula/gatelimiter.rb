167a5dd552a8f06f05606b96180b515bd75ecd64
Jaehoon-So <jhspacelover@naver.com>
2025-08-03 02:27:16 +0900

    fix: hashvalue

--- Formula/gatelimiter.rb
+++ Formula/gatelimiter.rb
@@ -5,7 +5,7 @@
     desc "A eng-kor dictionary for the terminal users"
     homepage "https://github.com/sapiensXXV/gate-limiter"
     url "https://github.com/sapiensXXV/gate-limiter/releases/download/0.1.0/gatelimiter.tar.gz"
-    sha256 "sha256:311403707a004ee376629e19f17f84adb3e6d2710c375c71a946bcc03ed856e8"
+    sha256 "311403707a004ee376629e19f17f84adb3e6d2710c375c71a946bcc03ed856e8"
     version "0.1.0"
 
     def install
