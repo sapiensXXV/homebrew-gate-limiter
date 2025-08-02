5ad2869eec389b3f6f7941a1cdf9cf377dea57aa
Jaehoon-So <jhspacelover@naver.com>
2025-08-03 02:30:45 +0900

    fix: rb file

--- Formula/gatelimiter.rb
+++ Formula/gatelimiter.rb
@@ -1,8 +1,8 @@
   # Documentation: https://docs.brew.sh/Formula-Cookbook
   #                https://rubydoc.brew.sh/Formula
   # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-  class Rsdic < Formula
-    desc "A eng-kor dictionary for the terminal users"
+  class Gatelimiter < Formula
+    desc "middleware to throttle client requests with multiple algorithms and custom settings"
     homepage "https://github.com/sapiensXXV/gate-limiter"
     url "https://github.com/sapiensXXV/gate-limiter/releases/download/0.1.0/gatelimiter.tar.gz"
     sha256 "311403707a004ee376629e19f17f84adb3e6d2710c375c71a946bcc03ed856e8"
